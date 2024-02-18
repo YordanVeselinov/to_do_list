3e76546873f9e9142dd4b53f3811522115af76d3
YordanVeselinov <yordan.r.veselinov@gmail.com>
2024-02-18 23:13:59 +0200

    Added If statments
    When a user selects a number we need to do something depending which option he selected.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 print("Welcome to my console To Do List!")
 
 while True:
@@ -7,4 +9,16 @@ while True:
     print(f"4. Exit")
 
     choice = input("Enter the number corresponding to the action (1-4): \n")
+
+    if choice == "4":
+        break
+    elif choice == "1":
+        pass
+    elif choice == "2":
+        pass
+    elif choice == "3":
+        pass
+    else:
+        print("Invalid number! Please enter number between 1 and 4.\n")
+        time.sleep(2)
 
