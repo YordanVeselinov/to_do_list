30f6c3a516d95e4f7cce68d2642c8b076a3399f8
YordanVeselinov <yordan.r.veselinov@gmail.com>
2024-02-18 21:20:00 +0200

    Added option for the user to chose from

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-print("Welcome to my console To Do List!\n")
-
+print("Welcome to my console To Do List!")
 
 while True:
-    pass
+    print(f"1. Add task")
+    print(f"2. Remove task")
+    print(f"3. View tasks")
+    print(f"4. Exit")
+
+    choice = input("Enter the number corresponding to the action (1-4): \n")
+
