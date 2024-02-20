eade047b4a82db224572f065d45f6daa0c0af977
YordanVeselinov <yordan.r.veselinov@gmail.com>
2024-02-20 12:13:05 +0200

    Fixed the prints

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import time
 
-print("Welcome to my console To Do List!")
+print("--- Welcome to my console To Do List! ---")
 
 while True:
-    print(f"1. Add task")
-    print(f"2. Remove task")
-    print(f"3. View tasks")
+    print(f"1. Add a task")
+    print(f"2. Remove a task")
+    print(f"3. View all tasks")
     print(f"4. Exit")
 
     choice = input("Enter the number corresponding to the action (1-4): \n")
