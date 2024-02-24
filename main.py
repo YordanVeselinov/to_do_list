a22648f1ea912c035897147e2749a96cbd14177c
YordanVeselinov <yordan.r.veselinov@gmail.com>
2024-02-24 20:31:31 +0200

    Added "add_task" and "show_tanks"
    added function when user select 2 or 3 options so they can add or view all of there tasks.

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 import time
 
+
+def add_task():
+    task = input("Enter your task: ")
+    with open("list_of_to_do.txt", "a") as file:
+        file.write(task + "\n")
+        print("Task added successfully!")
+
+    while True:
+        answer_to_add_more = input("Do you want more tasks? (Y or N): ").upper()
+
+        if answer_to_add_more == "N":
+            break
+        elif answer_to_add_more == "Y":
+            add_task()
+        else:
+            print("Invalid answer, please enter 'Y' or 'N'!")
+
+
+def show_tasks():
+    with open("list_of_to_do.txt") as file:
+        list_of_tasks = file.readlines()
+        for line in list_of_tasks:
+            number_of_task = list_of_tasks.index(line) + 1
+            print(f"{number_of_task}. " + line[:-1])
+
+    time.sleep(1)
+    print()
+
+    
 print("--- Welcome to my console To Do List! ---")
 
 while True:
@@ -20,12 +49,13 @@ while True:
         else:
             print("Invalid answer, please try again!")
     elif choice == "1":
-        pass
+        add_task()
     elif choice == "2":
         pass
     elif choice == "3":
-        pass
+        show_tasks()
     else:
         print("Invalid number! Please enter number between 1 and 4.\n")
         time.sleep(2)
+
 
